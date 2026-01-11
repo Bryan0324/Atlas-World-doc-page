6d83e9d3edc927efc3f0782b40291b025bf3ff81
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2026-01-11 15:47:16 +0800

    using mine url for testing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ryanx0621.github.io',
+  url: 'https://Bryan0324.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Atlas-World/',
